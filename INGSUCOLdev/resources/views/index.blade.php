d79b587441230d5689ca9092ef12a451a3652870
Sebastian Mora Office <mrsebastian64@gmail.com>
2022-04-05 16:02:16 -0500

    Pruebas modelo responsive - INGSUCOL

---
+++ INGSUCOLdev/resources/views/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.plantilla')
+
+
+@section('content')
+
+    <h1 class="font-medium text-zinc-400">Bienvenidos a INGSUCOL</h1>
+    <br>
+    <div class="w-full text-center grid grid-cols-2 ">
+        <div class="font-medium text-zinc-400">
+            Productos 1
+            <br>
+            <p class="text-sm font-normal text-zinc-400 sm:text-base"> Descripción breve del producto, precio y valoración. </p>
+        </div>
+        <div class="font-medium text-zinc-400">
+            Productos 2
+            <br>
+            <p class="text-sm font-normal text-zinc-400 sm:text-base"> Descripción breve del producto, precio y valoración. </p>
+        </div>
+    </div>
+    
+@endsection
