a68b9dc97757b8383120ff92fc11533e3a74e07c
Sebastian Mora Office <mrsebastian64@gmail.com>
2022-04-21 18:14:55 -0500

    cambio menor

--- project-prevycons/resources/views/novedades.blade.php
+++ project-prevycons/resources/views/novedades.blade.php
@@ -32,24 +32,24 @@
         <a href="#" class="flex flex-col items-center rounded border border-[#7398fd] shadow-md md:flex-row md:max-w-xl bg-gradient-to-t from-blue-700 to-[#001559]">
             <img class="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/docs/images/blog/image-4.jpg" alt="">
             <div class="flex flex-col justify-between p-4 leading-normal">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Salud ocupacional</h5>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight">Salud ocupacional</h5>
+                <p class="mb-3 font-normal">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
             </div>
         </a>
 
         <a href="#" class="flex flex-col items-center rounded border border-[#7398fd] shadow-md md:flex-row md:max-w-xl bg-gradient-to-t from-blue-700 to-[#001559]">
             <img class="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/docs/images/blog/image-4.jpg" alt="">
             <div class="flex flex-col justify-between p-4 leading-normal">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Salud ocupacional</h5>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight ">Salud ocupacional</h5>
+                <p class="mb-3 font-normal">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
             </div>
         </a>
 
         <a href="#" class="flex flex-col items-center rounded border border-[#7398fd] shadow-md md:flex-row md:max-w-xl bg-gradient-to-t from-blue-700 to-[#001559]">
             <img class="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/docs/images/blog/image-4.jpg" alt="">
             <div class="flex flex-col justify-between p-4 leading-normal">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Salud ocupacional</h5>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight ">Salud ocupacional</h5>
+                <p class="mb-3 font-normal">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
             </div>
         </a>
         
